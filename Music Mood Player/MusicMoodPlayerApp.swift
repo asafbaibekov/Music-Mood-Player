6db26dc50bb9655822b1d34ad0cd57c2dfbdb00c
Asaf Baibekov <asafall21@gmail.com>
2025-09-21 20:49:04 +0300

    Initial Commit

---
+++ Music Mood Player/MusicMoodPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicMoodPlayerApp.swift
+//  Music Mood Player
+//
+//  Created by Asaf Baibekov on 21/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct MusicMoodPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
