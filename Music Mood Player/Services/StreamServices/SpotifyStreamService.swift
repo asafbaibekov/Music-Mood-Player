95a63403eb1a4dddbddfe599e81c431951127f80
Asaf Baibekov <asafall21@gmail.com>
2025-12-05 21:30:04 +0200

    add tokenSwapURL and tokenRefreshURL and add didRenew

--- Music Mood Player/Services/StreamServices/SpotifyStreamService.swift
+++ Music Mood Player/Services/StreamServices/SpotifyStreamService.swift
@@ -24,6 +24,8 @@ final class SpotifyStreamService: NSObject, MusicStreamService {
             clientID: "2f4647040f594d49a3d0c8369090182c",
             redirectURL: URL(string: "musicmoodplayer://spotify-login-callback")!
         )
+        configuration.tokenSwapURL = URL(string: "https://u3irfuz4i3lbxzrux24zytjw7m0dwzva.lambda-url.eu-west-1.on.aws/")!
+        configuration.tokenRefreshURL = URL(string: "https://c7x2hf5hzrpsqvzw5int2t65pa0ncjgz.lambda-url.eu-west-1.on.aws/")!
         return SPTSessionManager(configuration: configuration, delegate: self)
     }()
     
@@ -58,5 +60,10 @@ extension SpotifyStreamService: SPTSessionManagerDelegate {
     func sessionManager(manager: SPTSessionManager, didFailWith error: any Error) {
         print(#function)
         self.isLoggedInSubject.value = false
+    }
+    
+    func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
+        print(#function)
+        self.isLoggedInSubject.value = true
     }
 }
